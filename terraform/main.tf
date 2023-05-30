5d207ef60482e56ec85f8607c4eda38d0af1b416
saugattiwarii <saugat.tiwari@certegy.com>
2023-05-30 12:48:14 +0545

    added prod, dev, uat and networking account using AFT

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,140 @@
-module "sandbox" {
+# module "sandbox" {
+#   source = "./modules/aft-account-request"
+
+#   control_tower_parameters = {
+#     AccountEmail              = "admin+sandbox@fictonica.com"
+#     AccountName               = "Fictonica Sandbox"
+#     ManagedOrganizationalUnit = "Learn AFT"
+#     SSOUserEmail              = "admin+sandbox@fictonica.com"
+#     SSOUserFirstName          = "Sandbox"
+#     SSOUserLastName           = "AFT"
+#   }
+
+#   account_tags = {
+#     "Learn Tutorial" = "AFT"
+#   }
+
+#   change_management_parameters = {
+#     change_requested_by = "HashiCorp Learn"
+#     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+#   }
+
+#   custom_fields = {
+#     group = "non-prod"
+#   }
+
+#   account_customizations_name = "sandbox"
+# }
+
+module "prod" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
+    AccountEmail              = "admin+prod@fictonica.com"
+    AccountName               = "Fictonica Prod"
+    ManagedOrganizationalUnit = "Fictonica Workload"
+    SSOUserEmail              = "admin+prod@fictonica.com"
+    SSOUserFirstName          = "Prod"
     SSOUserLastName           = "AFT"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    "ProvisionedVia" = "AFT"
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+    change_requested_by = "Fictonica DevOps Team"
+    change_reason       = "AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "prod"
+  }
+
+  account_customizations_name = "Fictonica Prod"
+}
+
+module "dev" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "admin+prod@fictonica.com"
+    AccountName               = "Fictonica Dev"
+    ManagedOrganizationalUnit = "Fictonica Workload"
+    SSOUserEmail              = "admin+dev@fictonica.com"
+    SSOUserFirstName          = "Dev"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "ProvisionedVia" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Fictonica DevOps Team"
+    change_reason       = "AWS Control Tower Account Factory for Terraform"
   }
 
   custom_fields = {
     group = "non-prod"
   }
 
-  account_customizations_name = "sandbox"
+  account_customizations_name = "Fictonica-Dev"
+}
+
+
+module "uat" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "admin+prod@fictonica.com"
+    AccountName               = "Fictonica UAT"
+    ManagedOrganizationalUnit = "Fictonica Workload"
+    SSOUserEmail              = "admin+uat@fictonica.com"
+    SSOUserFirstName          = "UAT"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "ProvisionedVia" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Fictonica DevOps Team"
+    change_reason       = "AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "Fictonica-UAT"
+}
+
+module "networking" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "admin+prod@fictonica.com"
+    AccountName               = "Fictonica Networking"
+    ManagedOrganizationalUnit = "Fictonica Workload"
+    SSOUserEmail              = "admin+networking@fictonica.com"
+    SSOUserFirstName          = "UAT"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "ProvisionedVia" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Fictonica DevOps Team"
+    change_reason       = "AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "Fictonica Networking"
 }
